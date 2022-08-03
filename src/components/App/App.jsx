2e053d8e56e680720e8d84fb56137469791de12a
Maxim Stribylo <max057521@gmail.com>
2022-08-03 23:33:40 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,16 @@
-import { Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Header } from 'components/Header/Header';
-import { Loader } from 'components/Loader/Loader';
-
-const HomePage = () =>
-  import('pages/HomePage' /* webpackChunkName: "home-page" */);
-const MoviesPage = () =>
-  import('pages/MoviesPage' /* webpackChunkName: "movies-page" */);
-const MovieDetailsPage = () =>
-  import('pages/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */);
-const Cast = () => import('pages/Cast' /* webpackChunkName: "Cast" */);
-
-const Reviews = () => import('pages/Reviews' /* webpackChunkName: "Reviews" */);
-
 export const App = () => {
   return (
-    <h2>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<HomePage />} />
-            <Route path="movies" element={<MoviesPage />} />
-            <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </h2>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,23 @@
+import { createPortal } from 'react-dom';
+import { Audio } from 'react-loader-spinner';
 
+const loaderRoot = document.querySelector('#loader-root');
+
+export const Loader = () => {
+  return createPortal(
+    <Audio
+      height={100}
+      width={100}
+      radius={9}
+      color="#00BFFF"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    />,
+    loaderRoot
+  );
+};
