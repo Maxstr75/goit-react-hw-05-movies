e1985643e28c25e2c0fd3db522e175d2738d29fd
Maxim Stribylo <max057521@gmail.com>
2022-08-03 23:57:57 +0300

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,19 @@
+import { Outlet } from 'react-router-dom';
 
+export const Header = () => {
+  return (
+    <>
+      <nav>
+        <container>
+          <wrapper>
+            <link to="/">Home</link>
+            <link to="/">Movies</link>
+          </wrapper>
+        </container>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
