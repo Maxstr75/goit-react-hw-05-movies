31bce4dedbb891ffdd724227fdd5b37a567c6af6
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-08-03 20:28:24 +0000

    Deploying to gh-pages from @ Maxstr75/goit-react-hw-05-movies@06f6618188ac686994591c9e1dd8ca43e4f81630 🚀

---
+++ static/js/Cast.05170d51.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[709],{493:function(){}}]);
---
+++ static/js/movie-details-page.60622d00.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[347],{602:function(){}}]);
