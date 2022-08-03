06f6618188ac686994591c9e1dd8ca43e4f81630
Maxim Stribylo <max057521@gmail.com>
2022-08-03 23:27:17 +0300

    add app routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,34 @@
-// import { Suspense } from 'react';
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import { Header } from '../Header';
-// import { Loader } from '../Loader';
+import { Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Header } from 'components/Header/Header';
+import { Loader } from 'components/Loader/Loader';
 
-// const HomePage = lazy(() =>
-//   import('pages/HomePage' /* webpackChunkName: "home-page" */)
-// );
+const HomePage = () =>
+  import('pages/HomePage' /* webpackChunkName: "home-page" */);
+const MoviesPage = () =>
+  import('pages/MoviesPage' /* webpackChunkName: "movies-page" */);
+const MovieDetailsPage = () =>
+  import('pages/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */);
+const Cast = () => import('pages/Cast' /* webpackChunkName: "Cast" */);
 
-// const MoviesPage = lazy(() =>
-//   import('pages/MoviesPage' /* webpackChunkName: "movies-page" */)
-// );
+const Reviews = () => import('pages/Reviews' /* webpackChunkName: "Reviews" */);
 
-// const MovieDetailsPage = lazy(() =>
-//   import('pages/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */)
-// );
-
-// const Cast = lazy(() => import('pages/Cast' /* webpackChunkName: "Cast" */));
-
-// const Reviews = lazy(() =>
-//   import('pages/Reviews' /* webpackChunkName: "Reviews" */)
-// );
-
-// export const App = () => {
-//   return (
-//     <h2>
-//       <Suspense fallback={<Loader />}>
-//         <Routes>
-//           <Route path="/" element={<Header />}>
-//             <Route index element={<HomePage />} />
-//             <Route path="movies" element={<MoviesPage />} />
-//             <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-//               <Route path="cast" element={<Cast />} />
-//               <Route path="reviews" element={<Reviews />} />
-//             </Route>
-//             <Route path="*" element={<Navigate to="/" />} />
-//           </Route>
-//         </Routes>
-//       </Suspense>
-//     </h2>
-//   );
-// };
+export const App = () => {
+  return (
+    <h2>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<HomePage />} />
+            <Route path="movies" element={<MoviesPage />} />
+            <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </h2>
+  );
+};
